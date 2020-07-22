3a98e4f4d660a1899a3c774e7bdc6e55e15abb41
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-07-22 17:34:39 +0100

    displayed beers, set up views

--- app.js
+++ app.js
@@ -13,10 +13,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 // ...
 
 // Add the route handlers here:
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromAPI => {
+      res.render('beers', { beers: beersFromAPI });
+    })
+    .catch(error => console.log('error'));
+});
+
+app.get('/random-beers', (req, res) => {
+  res.render('random-beers');
+});
 
 app.get('/', (req, res) => {
   res.render('index');
